143ea6340739e5a24c80a15b46b6fb2cddc10d74
Peter Hwang <nam-joong.hwang@yale.edu>
2019-05-22 05:27:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,9 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  # Your code here
+  new_array = array.collect do |planet|
+    put planet.capitalize"!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
