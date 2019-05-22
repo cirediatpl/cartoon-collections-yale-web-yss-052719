3e3a734f7c908b22769d306cd901af92c76c0b15
Peter Hwang <nam-joong.hwang@yale.edu>
2019-05-22 05:35:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,4 +18,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.include?(cheese_types[0])
+  array.include?(cheese_types[1])
+  array.include?(cheese_types[2])
 end
